cd62e829bae1d198c824fe465237bff040c2bc0a
Christopher Alford <chrisvalford@me.com>
2022-04-01 11:31:17 +0200

    Moved to library

--- VeriffI/Scenes/Home/HomeViewController.swift
+++ VeriffI/Scenes/Home/HomeViewController.swift
@@ -6,6 +6,7 @@ View controller from which to invoke the document scanner.
 */
 
 import UIKit
+import VeriffLib
 import VisionKit
 import Vision
 
@@ -13,29 +14,12 @@ class HomeViewController: UIViewController {
     
     @IBOutlet weak var scanDocumentButton: UIButton!
     @IBOutlet weak var facelandmarkButton: UIButton!
-    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var noCameraView: NoCameraView!
     
-    static let faceLandmarkIdentifier = "faceLandmarkVC"
-    static let scanDocumentIdentifier = "scanDocumentVC"
-
-    enum ScanMode: Int, CustomStringConvertible {
-        case face = 0
-        case document
-        
-        var description: String {
-            switch self {
-            case .face:
-                return "faceLandmarkVC"
-            case .document:
-                return "scanDocumentVC"
-            }
-        }
-    }
-    
-    var scanMode: ScanMode = .document
-    var resultsViewController: (UIViewController & RecognizedTextDataSource)?
-    var textRecognitionRequest = VNRecognizeTextRequest()
+    lazy var libStoryboard: UIStoryboard = {
+        let storyboard = UIStoryboard(name: "SDK", bundle: VeriffLib.libBundle)
+        return storyboard
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,86 +32,16 @@ class HomeViewController: UIViewController {
             noCameraView.isHidden = false
             noCameraView.layoutSubviews()
         }
-        
-        textRecognitionRequest = VNRecognizeTextRequest(completionHandler: { (request, error) in
-            guard let resultsViewController = self.resultsViewController else {
-                print("resultsViewController is not set")
-                return
-            }
-            if let results = request.results, !results.isEmpty {
-                if let requestResults = request.results as? [VNRecognizedTextObservation] {
-                    DispatchQueue.main.async {
-                        resultsViewController.addRecognizedText(recognizedText: requestResults)
-                    }
-                }
-            }
-        })
-        // This doesn't require OCR on a live camera feed, select accurate for more accurate results.
-        textRecognitionRequest.recognitionLevel = .accurate
-        textRecognitionRequest.usesLanguageCorrection = true
-    }
-
-    @IBAction func scan(_ sender: UIControl) {
-        guard let scanMode = ScanMode(rawValue: sender.tag) else { return }
-        self.scanMode = scanMode
-        if scanMode == .document {
-            let documentCameraViewController = VNDocumentCameraViewController()
-            documentCameraViewController.delegate = self
-            present(documentCameraViewController, animated: true)
-        } else {
-            // "faceLandmarkVC"
-            guard let faceLandmarkVC = self.storyboard?.instantiateViewController(withIdentifier: ScanMode.face.description) else {
-                return
-            }
-            self.present(faceLandmarkVC, animated: true)
-        }
     }
     
-    func processImage(image: UIImage) {
-        guard let cgImage = image.cgImage else {
-            print("Failed to get cgimage from input image")
-            return
-        }
-        
-        let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
-        do {
-            try handler.perform([textRecognitionRequest])
-        } catch {
-            print(error)
-        }
+    @IBAction func documentTapped(_ sender: UIButton) {
+        guard let scanDocumentViewController = libStoryboard.instantiateViewController(withIdentifier: "scanDocumentViewController") as? ScanDocumentViewController else { return }
+        navigationController?.pushViewController(scanDocumentViewController, animated: true)
     }
-}
-
-extension HomeViewController: VNDocumentCameraViewControllerDelegate {
-    func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
-        var vcID: String?
+    
+    @IBAction func faceTapped(_ sender: UIButton) {
+        guard let faceDetectionViewController = libStoryboard.instantiateViewController(withIdentifier: "faceDetectionViewController") as? ScanFaceDetectionViewController else { return }
+        navigationController?.pushViewController(faceDetectionViewController, animated: true)
         
-        //TODO: replace with ScanMode enum description
-        switch scanMode {
-        case .face:
-            vcID = HomeViewController.faceLandmarkIdentifier
-        default:
-            vcID = HomeViewController.scanDocumentIdentifier
-        }
-        
-        if let vcID = vcID {
-            resultsViewController = storyboard?.instantiateViewController(withIdentifier: vcID) as? (UIViewController & RecognizedTextDataSource)
-        }
-        
-        self.activityIndicator.startAnimating()
-        controller.dismiss(animated: true) {
-            DispatchQueue.global(qos: .userInitiated).async {
-                for pageNumber in 0 ..< scan.pageCount {
-                    let image = scan.imageOfPage(at: pageNumber)
-                    self.processImage(image: image)
-                }
-                DispatchQueue.main.async {
-                    if let resultsVC = self.resultsViewController {
-                        self.navigationController?.pushViewController(resultsVC, animated: true)
-                    }
-                    self.activityIndicator.stopAnimating()
-                }
-            }
-        }
     }
 }
