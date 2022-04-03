7cae9ad6adac051bd53d0d3975888025c0e4830d
Christopher Alford <chrisvalford@me.com>
2022-04-03 12:17:18 +0200

    Added validation button and library delegate functions

--- VeriffI/Scenes/Home/HomeViewController.swift
+++ VeriffI/Scenes/Home/HomeViewController.swift
@@ -12,8 +12,11 @@ import Vision
 
 class HomeViewController: UIViewController {
     
+    @IBOutlet weak var scanDocumentImage: UIImageView!
     @IBOutlet weak var scanDocumentButton: UIButton!
-    @IBOutlet weak var facelandmarkButton: UIButton!
+    @IBOutlet weak var scanFaceImage: UIImageView!
+    @IBOutlet weak var scanFaceButton: UIButton!
+    @IBOutlet weak var startValidation: UIButton!
     @IBOutlet weak var noCameraView: NoCameraView!
     
     lazy var libStoryboard: UIStoryboard = {
@@ -21,12 +24,18 @@ class HomeViewController: UIViewController {
         return storyboard
     }()
     
+    private var faceImage: Data?
+    private var documentImage: Data?
+    private var landmarks: VNFaceObservation?
+    private var texts: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if !UIImagePickerController.isSourceTypeAvailable(.camera) {
             scanDocumentButton.isEnabled = false
-            facelandmarkButton.isEnabled = false
+            scanFaceButton.isEnabled = false
+            startValidation.isEnabled = false
             noCameraView.title = "Your device doesn't have a camera."
             noCameraView.message = "You need a camera to scan your documents and face."
             noCameraView.isHidden = false
@@ -38,6 +47,7 @@ class HomeViewController: UIViewController {
         guard let scanDocumentViewController = libStoryboard.instantiateViewController(withIdentifier: "scanDocumentViewController") as? ScanDocumentViewController else { return }
         scanDocumentViewController.modalPresentationStyle = .fullScreen
         scanDocumentViewController.modalTransitionStyle = .coverVertical
+        scanDocumentViewController.delegate = self
         show(scanDocumentViewController, sender: self)
         //self.present(scanDocumentViewController, animated: false, completion: nil)
     }
@@ -46,6 +56,63 @@ class HomeViewController: UIViewController {
         guard let scanFaceViewController = libStoryboard.instantiateViewController(withIdentifier: "scanFaceViewController") as? ScanFaceViewController else { return }
         scanFaceViewController.modalPresentationStyle = .fullScreen
         scanFaceViewController.modalTransitionStyle = .coverVertical
+        scanFaceViewController.delegate = self
         self.present(scanFaceViewController, animated: true, completion: nil)
+    }
+    
+    @IBAction func validateTapped(_ sender: UIButton) {
+        var documentValidated = false
+        var faceValidated = false
+        
+        guard let data = faceImage, let landmarks = landmarks else {
+            return
+        }
+        VerifyRemote.verifyIdentity(imageData: data, landMarks: landmarks, completion: { (state, error) in
+            if state == .verified {
+                print("Face recognized Ok")
+                faceValidated = true
+                if faceValidated && documentValidated {
+                    self.startValidation.setTitle("Validated ✔️", for: .normal)
+                }
+            }
+        })
+        
+        guard let data = documentImage else {
+            return
+        }
+        VerifyRemote.verifyDocument(imageData: data, texts: texts, completion: { (state, error) in
+            if state == .verified {
+                print("Document recognized Ok")
+                documentValidated = true
+                if faceValidated && documentValidated {
+                    self.startValidation.setTitle("Validated ✔️", for: .normal)
+                }
+            }
+        })
+    }
+    
+    private func shouldEnableValidation() {
+        guard let _ = faceImage, let _ = documentImage else {
+            return
+        }
+        startValidation.isEnabled = true
+    }
+}
+
+extension HomeViewController: ScanFaceDelegate {
+    func scanCompleted(imageData: Data, landmarks: VNFaceObservation) {
+        faceImage = imageData
+        self.landmarks = landmarks
+        scanFaceImage.image = UIImage(data: imageData)
+        shouldEnableValidation()
+    }
+}
+
+extension HomeViewController: ScanDocumentDelegate {
+    func scanCompleted(imageData: Data, texts: [String]) {
+        documentImage = imageData
+        self.texts = texts
+        scanDocumentImage.image = UIImage(data: imageData)
+        shouldEnableValidation()
     }
 }
